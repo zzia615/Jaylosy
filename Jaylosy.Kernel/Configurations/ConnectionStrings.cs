12a34245ef42ffe41dac6a8d9324f9713023c550
jiaolong <jackjone@me.com>
2018-11-08 14:31:57 +0800

    新增数据库字符串类

---
+++ Jaylosy.Kernel/Configurations/ConnectionStrings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace Jaylosy.Kernel.Configurations
+{
+    public class ConnectionStrings
+    {
+        protected string this[string name]
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+            }
+        }
+    }
+}
